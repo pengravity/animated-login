032e3b30d0f2421806fd5d03fbb319892bbe71f7
Andy <alipinski@mail.com>
2022-10-15 15:07:09 +0200

    reset password component

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,13 +1,15 @@
 import './AuthContainer.scss';
 import Login from './Login';
 import Register from './Register';
+import Reset from './Reset';
 
 const AuthContainer = () => {
   return (
     <section className='--flex-center --100vh  '>
       <div className='container box'>
         {/*<Login /> */}
-        <Register />
+        {/*<Register /> */}
+        <Reset />
       </div>
     </section>
   );
--- src/components/auth/Reset.js
+++ src/components/auth/Reset.js
@@ -1,7 +1,29 @@
+import ResetImg from '../../assets/forgot.svg';
+import { SlClose } from 'react-icons/sl';
+
 const Reset = () => {
   return (
-    <div>
-      <h1>Reset</h1>
+    <div className='main-container --flex-center'>
+      <div className='form-container reset'>
+        <form className='--form-control'>
+          <h2 className='--color-danger  --text-center'>Reset Password</h2>
+          <input type='email' className='--width-100' placeholder='Email' />
+
+          <button className='--btn --btn-primary --btn-block'>
+            Reset Password
+          </button>
+
+          <span className='--text-sm --block --text-center'>
+            We will send you a reset link!
+          </span>
+          <div className='close'>
+            <SlClose className='--color-danger' size={32} />
+          </div>
+        </form>
+      </div>
+      <div className='img-container'>
+        <img src={ResetImg} alt='login' />
+      </div>
     </div>
   );
 };
